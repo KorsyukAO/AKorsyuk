38852ab7187351536caffba92cf690fc17ac2cf8
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-01-14 10:40:33 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+ *Class Calculate решение задачи 1.1. Создание Hello World.
+ *@autor AKorsyuk
+ *@since 13.01.2018
+ *@version 0.1
+*/
 public class Calculate {
+	/**
+	*Конструктор, вывод строки в консоль.
+	*@param args - args
+	*/
 	public static void main(String[] args) {
 		System.out.println ("Hello world.");
 	}
