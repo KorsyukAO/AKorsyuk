f03fdc7b487520a198453479a775fa8e0c5bbda4
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-01-14 13:32:30 +0300

    Errors 1.4 тест Hello world.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,9 @@
 package ru.job4j;
 /**
  *Class Calculate решение задачи 1.1. Создание Hello World.
- *@autor AKorsyuk
+ *@autor Alexandr Korsyuk (Korsyuk@gmail.com)
  *@since 13.01.2018
- *@version 0.1
+ *@version 0.2
 */
 public class Calculate {
 	/**
@@ -12,5 +12,14 @@ public class Calculate {
 	*/
 	public static void main(String[] args) {
 		System.out.println ("Hello world.");
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 }
