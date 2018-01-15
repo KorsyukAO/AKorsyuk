aa84636e0f92083c9cada2d81ae81257e7788b24
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-01-15 22:18:32 +0300

    2. Импорт Maven проекта. [#31168] (add .gitignore)

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -12,33 +12,33 @@ import static org.junit.Assert.assertThat;
  *@autor Alexandr Korsyuk (Korsyuk@gmail.com)
  *@since 14.01.2018
  *@version 0.1
-*/
+ */
 public class CalculateTest {
 	/**
-	*Test add.
-	*/
+	 *Test add.
+	 */
 	@Test
-	 public void whenAddOneToOneThenTwo() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        Calculate.main(null);
-        assertThat(
-                out.toString(),
-                is(
-                        String.format(
-                                "Hello world.\r\n",
-                                System.getProperty("line.separator")
-                        )
-                )
-        );
+	public void whenAddOneToOneThenTwo() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(
+				out.toString(),
+				is(
+						String.format(
+								"Hello world.\r\n",
+								System.getProperty("line.separator")
+						)
+				)
+		);
 	}
-	
+
 	/**
-	* Test echo.
-	*/ @Test
+	 * Test echo.
+	 */ @Test
 	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Alexandr Korsyuk";
-		String expect = "Echo, echo, echo : Alexandr Korsyuk"; 
+		String expect = "Echo, echo, echo : Alexandr Korsyuk";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
