d3a3b6e27801a25165213d96657395cb3d15fc7f
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-01-17 21:42:52 +0300

    3.2. Максимум из двух чисел [#189] (add Max and MaxTest)

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.max;
+/**
+ *Class Max решение задачи 3.2. Максимум из двух чисел.
+ *@autor Alexandr Korsyuk (Korsyuk@gmail.com).
+ *@since 17.01.201
+ *@version 0.1
+ */
+public class Max {
+    public int max(int ferst, int secomd) {
+        return ferst > secomd ? ferst : secomd;
+    }
+}
