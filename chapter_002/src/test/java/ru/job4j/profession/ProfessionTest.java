a6546f7a6e5e14e3d1f3560d737b03e763ddff93
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-01-28 20:16:13 +0300

    refactoring package (Test)

--- chapter_002/src/test/java/ru/job4j/profession/ProfessionTest.java
+++ chapter_002/src/test/java/ru/job4j/profession/ProfessionTest.java
@@ -1,4 +1,4 @@
-package ru.jpb4j.profession;
+package ru.job4j.profession;
 
 import org.junit.Test;
 import ru.job4j.profession.Doctor;
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertThat;
  */
 public class ProfessionTest {
     @Test
-    public void whenDoctorHealTeacher(){
+    public void whenDoctorHealTeacher() {
         Doctor doctor = new Doctor("Норик", 29, "23.03.1988 ", "Диплом врача");
         Teacher teacher = new Teacher("Болик", 28, "21.04.1989", "Диплом учителя");
         String result = doctor.diagnoseHeal(teacher);
