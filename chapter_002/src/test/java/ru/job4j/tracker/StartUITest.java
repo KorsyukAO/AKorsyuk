f25c30a757c955ac4ee78be9d9ce15fdbe3be08e
Alexandr Korsyuk <Korsyuk.ao@gmail.com>
2018-02-19 23:18:02 +0300

    002.4.6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,20 +1,44 @@
 package ru.job4j.tracker;
 
 import ru.job4j.tracker.start.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    private Tracker tracker = new Tracker();
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
     /**
      * 0. Создание новой заявки.
      */
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();     // создаём Tracker
-        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
-        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
-        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        //создаём StubInput с последовательностью действий
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        //   создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(tracker.getAll()[0].getName(), is("test name"));
     }
 
     /**
@@ -22,8 +46,6 @@ public class StartUITest {
      */
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
-        // создаём Tracker
-        Tracker tracker = new Tracker();
         //Напрямую добавляем заявку
         Item item = tracker.add(new Item());
         //создаём StubInput с последовательностью действий
@@ -33,13 +55,11 @@ public class StartUITest {
         // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
         assertThat(tracker.findById(item.getId()).getName(), is("test name"));
     }
-
     /**
      * 3. Удаление заявки.
      */
     @Test
     public void whenDeleteItemsThenTracker() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("name", "name"));
         Item item1 = tracker.add(new Item("name1", "name1"));
         Item item2 = tracker.add(new Item("name2", "name2"));
@@ -47,5 +67,43 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"1", "3", item.getId(), "1", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll(), is(result));
+    }
+    /**
+     * 1. Вывод всех заявок из хранилища.
+     */
+    @Test
+    public void whenOutputAllItems() {
+        Item item = tracker.add(new Item("name", "name"));
+        Item item1 = tracker.add(new Item("name1", "name1"));
+        Item item2 = tracker.add(new Item("name2", "name2"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("0. Создание новой заявки.")
+                                .append(System.lineSeparator()).append("1. Вывод всех заявок на экран.")
+                                .append(System.lineSeparator()).append("2. Редоктирование заявки.")
+                                .append(System.lineSeparator()).append("3. Удаление заявки.")
+                                .append(System.lineSeparator()).append("4. Поиск заявки по ID.")
+                                .append(System.lineSeparator()).append("5. Поиск заявки по имени.")
+                                .append(System.lineSeparator()).append("6. Выход из программы.")
+                                .append(System.lineSeparator()).append("------------ Вывод всех заявок --------------")
+                                .append(System.lineSeparator()).append("Заявка: ID: " + item.getId() + "   Имя: name                 Описание: name                ")
+                                .append(System.lineSeparator()).append("Заявка: ID: " + item1.getId() + "   Имя: name1                Описание: name1               ")
+                                .append(System.lineSeparator()).append("Заявка: ID: " + item2.getId() + "   Имя: name2                Описание: name2               ")
+                                .append(System.lineSeparator()).append("------------ Вывод всех заявок --------------")
+                                .append(System.lineSeparator()).append("0. Создание новой заявки.")
+                                .append(System.lineSeparator()).append("1. Вывод всех заявок на экран.")
+                                .append(System.lineSeparator()).append("2. Редоктирование заявки.")
+                                .append(System.lineSeparator()).append("3. Удаление заявки.")
+                                .append(System.lineSeparator()).append("4. Поиск заявки по ID.")
+                                .append(System.lineSeparator()).append("5. Поиск заявки по имени.")
+                                .append(System.lineSeparator()).append("6. Выход из программы.")
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
     }
 }
